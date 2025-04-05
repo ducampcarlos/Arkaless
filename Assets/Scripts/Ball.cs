40a2dcc4d734d8a4e806d2085da42dd44564c4a3
ducampcarlos <ducampcarlos@gmail.com>
2025-04-05 15:03:30 -0300

    Added: Ball movement.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartBounce();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartBounce()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        rb.AddForce(randomDirection * 10f, ForceMode2D.Impulse);
+    }
+}
